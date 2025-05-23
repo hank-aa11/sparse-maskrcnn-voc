5de68b10e422a7cece958b19e2ccb962a6039b74
JICHUAN <1903085342@qq.com>
2025-05-23 10:31:11 +0800

    Create setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,20 @@
+# 设置MMDetection VOC实验所需的环境
+# 1. 创建并激活Conda环境
+conda create -n openmmlab-voc python=3.9 -y
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate openmmlab-voc
+
+# 2. 安装PyTorch (cu121, torch2.3.0)
+pip3 install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu121
+pip install tensorboard future
+
+# 3. 安装MMEngine和MMCV
+pip install -U openmim
+mim install mmengine
+pip install "mmcv<2.2.0,>=2.0.0rc4"
+
+# 4. 克隆并安装MMDetection
+git clone https://github.com/open-mmlab/mmdetection.git
+cd mmdetection
+pip install -v -e.‘
+cd..
