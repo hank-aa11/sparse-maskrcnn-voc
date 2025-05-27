7d7653d831bff7ea919d60fbe0dba35061030ef6
JICHUAN <1903085342@qq.com>
2025-05-27 16:36:55 +0800

    Update schedule_sparse_1x.py

--- schedule_sparse_1x.py
+++ schedule_sparse_1x.py
@@ -1,11 +1,9 @@
-# training schedule for 1x
 train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=60, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
-# learning rate
 param_scheduler = [
-    dict(type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=1000),  # warm-up
+    dict(type='LinearLR', start_factor=0.001, by_epoch=False, begin=0, end=1000), 
     dict(
         type='MultiStepLR',
         begin=0,
@@ -16,15 +14,10 @@ param_scheduler = [
 ]
 
 
-# optimizer
 optim_wrapper = dict(
     type='OptimWrapper',
-    optimizer=dict(type='AdamW', lr=2e-5, weight_decay=0.05),  # Sparse R-CNN 常使用 AdamW
+    optimizer=dict(type='AdamW', lr=2e-5, weight_decay=0.05),  
     clip_grad=dict(max_norm=1, norm_type=2)
 )
 
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (2 samples per GPU).
 auto_scale_lr = dict(enable=True, base_batch_size=16)
